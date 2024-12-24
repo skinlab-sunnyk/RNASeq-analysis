0a235e5dc1c93c6106438581f3d23aa80593a6e4
Sunny Kataria, PhD <80887310+skinlab-sunnyk@users.noreply.github.com>
2024-12-24 18:08:14 +0530

    Add files via upload
    Commands to generate kallisto output

---
+++ KalignBin.bat
@@ -0,0 +1,7 @@
+kallisto quant -i D:\RefGenomes\Mouse_ensembl\Mus_musculus.GRCm39.cdna.all.index -o D:\test\115 -t 8 115_1.fq.gz 115_2.fq.gz  1>D:\test\115.log 2>&1
+kallisto quant -i D:\RefGenomes\Mouse_ensembl\Mus_musculus.GRCm39.cdna.all.index -o D:\test\36 -t 8 36_1.fq.gz 36_2.fq.gz  1>D:\test\36.log 2>&1
+kallisto quant -i D:\RefGenomes\Mouse_ensembl\Mus_musculus.GRCm39.cdna.all.index -o D:\test\37 -t 8 37_1.fq.gz 37_2.fq.gz  1>D:\test\37.log 2>&1
+kallisto quant -i D:\RefGenomes\Mouse_ensembl\Mus_musculus.GRCm39.cdna.all.index -o D:\test\38 -t 8 38_1.fq.gz 38_2.fq.gz  1>D:\test\38.log 2>&1
+kallisto quant -i D:\RefGenomes\Mouse_ensembl\Mus_musculus.GRCm39.cdna.all.index -o D:\test\39 -t 8 39_1.fq.gz 39_2.fq.gz  1>D:\test\39.log 2>&1
+kallisto quant -i D:\RefGenomes\Mouse_ensembl\Mus_musculus.GRCm39.cdna.all.index -o D:\test\49 -t 8 49_1.fq.gz 49_2.fq.gz  1>D:\test\49.log 2>&1
+
